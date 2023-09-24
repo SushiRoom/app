c471492e175681a3130a99de1b11f7124c2d1735
aleeeee1 <pace.alessandro05@protonmail.com>
2023-09-24 16:34:59 +0200

    added a demo qr code on a created room

--- lib/ui/pages/create.dart
+++ lib/ui/pages/create.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sushi_room/models/room.dart';
+import 'package:sushi_room/services/rooms_api.dart';
 
 class CreatePage extends StatefulWidget {
   const CreatePage({super.key});
@@ -8,11 +12,33 @@ class CreatePage extends StatefulWidget {
 }
 
 class _CreatePageState extends State<CreatePage> {
+  RoomsAPI roomsAPI = RoomsAPI();
+  String uid = FirebaseAuth.instance.currentUser!.uid;
+
+  createNewRoom() async {
+    Room room = Room(
+      name: 'Test Room',
+      usesLocation: false,
+      creator: uid,
+    );
+
+    var roomId = await roomsAPI.createRoom(room);
+    debugPrint('Room created with id: $roomId');
+
+    Get.toNamed('/room', arguments: [roomId]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Create new Room'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: createNewRoom,
+          child: const Text('Create'),
+        ),
       ),
     );
   }
