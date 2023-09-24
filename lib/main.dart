38ca74cf13fcfafd40e55614764b47d83d588fb4
aleeeee1 <pace.alessandro05@protonmail.com>
2023-09-24 10:29:40 +0200

    added anonymous signin

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -23,6 +24,8 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  await FirebaseAuth.instance.signInAnonymously();
 
   runApp(const SushiRoom());
 }
