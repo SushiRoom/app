58d10ad85c1e2b08d61d911af5fa6ef9b7500e14
aleeeee1 <pace.alessandro05@protonmail.com>
2023-09-28 18:51:52 +0200

    added possiblity to manually put the room code

--- lib/ui/pages/scan_code.dart
+++ lib/ui/pages/scan_code.dart
@@ -16,57 +16,122 @@ class _ScanCodePageState extends State<ScanCodePage> {
   MobileScannerController controller = MobileScannerController();
   RoomsAPI roomsAPI = RoomsAPI();
 
+  bool manualInput = false;
+  TextEditingController textEditingController = TextEditingController();
+
+  showDialog(Room room) {
+    Get.dialog(
+      AlertDialog(
+        title: const Text('Room found!'),
+        content: Column(
+          children: [
+            Text("Room name: ${room.name}"),
+            Text("Password protected: ${room.password != null}"),
+          ],
+        ),
+        actions: [
+          OutlinedButton(
+            onPressed: () {
+              Get.back();
+            },
+            child: const Text("Retry"),
+          ),
+          FilledButton(
+            onPressed: () {
+              controller.stop();
+              Get.back();
+              Get.offAndToNamed(RouteGenerator.roomPageRoute, arguments: [room.id]);
+            },
+            child: const Text("Join"),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: MobileScanner(
-        controller: controller,
-        onDetect: (capture) async {
-          for (var element in capture.barcodes) {
-            if (element.format == BarcodeFormat.qrCode && element.rawValue != null) {
-              if (Get.isDialogOpen ?? false) {
-                return;
-              }
-
-              Room? room;
-              try {
-                room = await roomsAPI.getRoom(element.rawValue!);
-              } catch (e) {
-                return;
-              }
-
-              Get.dialog(
-                AlertDialog(
-                  title: const Text('Room found!'),
-                  content: Column(
-                    children: [
-                      Text("Room name: ${room.name}"),
-                      Text("Password protected: ${room.password != null}"),
-                    ],
-                  ),
-                  actions: [
-                    OutlinedButton(
-                      onPressed: () {
-                        Get.back();
-                      },
-                      child: const Text("Retry"),
-                    ),
-                    FilledButton(
-                      onPressed: () {
-                        controller.stop();
-                        Get.back();
-                        Get.offAndToNamed(RouteGenerator.roomPageRoute, arguments: [element.rawValue]);
-                      },
-                      child: const Text("Join"),
-                    ),
-                  ],
-                ),
-              );
-            }
-          }
-        },
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                manualInput = !manualInput;
+                if (manualInput == true) {
+                  controller.stop();
+                } else {
+                  controller.start();
+                }
+              });
+            },
+            icon: const Icon(Icons.keyboard),
+          ),
+        ],
       ),
+      body: !manualInput
+          ? MobileScanner(
+              controller: controller,
+              onDetect: (capture) async {
+                for (var element in capture.barcodes) {
+                  if (element.format == BarcodeFormat.qrCode && element.rawValue != null) {
+                    if (Get.isDialogOpen ?? false) {
+                      return;
+                    }
+
+                    Room? room;
+                    try {
+                      room = await roomsAPI.getRoom(element.rawValue!);
+                    } catch (e) {
+                      return;
+                    }
+
+                    showDialog(room);
+                  }
+                }
+              },
+            )
+          : Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(
+                    width: 200,
+                    child: TextField(
+                      controller: textEditingController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Room code',
+                        isDense: true,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () async {
+                      Room? room;
+                      try {
+                        room = await roomsAPI.getRoom(textEditingController.text);
+                      } catch (e) {
+                        if (!Get.isSnackbarOpen) {
+                          Get.snackbar(
+                            "Error",
+                            "Room not found",
+                            snackPosition: SnackPosition.BOTTOM,
+                            backgroundColor: Theme.of(context).colorScheme.error,
+                            colorText: Theme.of(context).colorScheme.onError,
+                          );
+                        }
+                        return;
+                      }
+
+                      showDialog(room);
+                    },
+                    child: const Text("Join"),
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
