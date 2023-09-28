72f327614c25d889dd6241691349dab22f216b2d
Zubby <mrgabrielborgo@gmail.com>
2023-09-28 23:20:44 +0200

    'room_landing'Page finished and minor tweaks

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ The purpose of the app is to provide a simple way to take orders foir kdosajf ad
 
 
 
+###### se non ti trova niente vicino, ti chiede se vuoi creare na stanza daje roma<daje
--- lib/ui/pages/room/room_landing.dart
+++ lib/ui/pages/room/room_landing.dart
@@ -20,69 +20,143 @@ class _RoomLandingState extends State<RoomLanding> with AutomaticKeepAliveClient
   Widget build(BuildContext context) {
     super.build(context);
 
-    return Column(
-      children: [
-        QrImageView(
-          data: widget.roomId,
-          backgroundColor: Colors.white,
-          size: 90,
-        ),
-        TextButton(
-          child: const Text("Copy room id"),
-          onPressed: () {
-            Clipboard.setData(
-              ClipboardData(text: widget.roomId),
-            );
-          },
-        ),
-        StreamBuilder(
-          stream: FirebaseDatabase.instance.ref().child('rooms').child(widget.roomId).onValue,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              Map<String, dynamic> roomData = (snapshot.data!.snapshot.value as Map).cast<String, dynamic>();
-              Room room = Room.fromJson(roomData);
-
-              return Column(
-                children: [
-                  Card(
-                    elevation: 1,
-                    child: SizedBox(
-                      height: 300,
-                      width: 350,
-                      child: Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Column(
+    return Scaffold(
+      body: Column(
+        children: [
+          StreamBuilder(
+            stream: FirebaseDatabase.instance.ref().child('rooms').child(widget.roomId).onValue,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                Map<String, dynamic> roomData = (snapshot.data!.snapshot.value as Map).cast<String, dynamic>();
+                Room room = Room.fromJson(roomData);
+                // Text("Table's plates: ${room.plates.length}"),
+                // Text("Password? ${room.password != null ? "Yes" : "No"}"),
+                // Text("Created by: ${room.users.firstWhere((element) => element.uid == room.creator).name}"),
+                return Column(
+                  children: [
+                    Row(
+                      children: [
+                        Flexible(
+                            child: Column(
                           children: [
-                            for (var user in room.users)
-                              ListTile(
-                                // add icon
-                                leading: const Icon(Icons.person),
-                                title: Text(user.name),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10, right: 10, top: 5),
+                              child: ListTile(
+                                leading: room.password == null ? const Icon(Icons.lock_open) : const Icon(Icons.lock_outlined),
+                                title: Text("Created by: ${room.users.firstWhere((element) => element.uid == room.creator).name}"),
+                                subtitle: Text("Table's plates: ${room.plates.length}"),
+                                trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    const Icon(
+                                      Icons.people,
+                                      size: 20,
+                                    ),
+                                    const SizedBox(width: 5),
+                                    Text(
+                                      room.users.length.toString(),
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
-                            TextButton.icon(
-                              icon: const Icon(Icons.add),
-                              onPressed: () => {},
-                              label: const Text("Add user"),
-                            )
+                            ),
                           ],
+                        )),
+                      ],
+                    ),
+                    Card(
+                      elevation: 1,
+                      child: SizedBox(
+                        height: 300,
+                        width: 350,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Column(
+                            children: [
+                              const Padding(
+                                padding: EdgeInsets.all(5.0),
+                                child: Text(
+                                  "Table",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ),
+                              for (var user in room.users)
+                                ListTile(
+                                    // add icon
+                                    leading: const Icon(Icons.person),
+                                    title: Text(user.name),
+                                    trailing: user.uid == room.creator
+                                        ? const Icon(
+                                            Icons.star_rounded,
+                                          )
+                                        : null),
+                              // TextButton.icon(
+                              //   icon: const Icon(Icons.add),
+                              //   onPressed: () => {},
+                              //   label: const Text("Add user"),
+                              // )
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-
-                  // Text("Room name: ${room.name}"),
-                  // Text("Partecipants: ${room.users.length}"),
-                  // for (var user in room.users) Text(user.name),
-                ],
-              );
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-        ),
-      ],
+                  ],
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {
+          showModalBottomSheet<void>(
+              context: context,
+              builder: (BuildContext context) {
+                return SizedBox(
+                  height: 300,
+                  child: Center(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 20.0),
+                        child: Text(
+                          "Scan to join the room",
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      QrImageView(
+                        data: widget.roomId,
+                        backgroundColor: Colors.white,
+                        size: 120,
+                      ),
+                      TextButton(
+                        child: const Text("Copy room id"),
+                        onPressed: () {
+                          Clipboard.setData(
+                            ClipboardData(text: widget.roomId),
+                          );
+                        },
+                      ),
+                    ],
+                  )),
+                );
+              })
+        },
+        child: const Icon(Icons.share),
+      ),
     );
   }
 }
