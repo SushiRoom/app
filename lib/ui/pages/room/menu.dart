842150d24ec0351860e8f90604c86961f6b3beb1
aleeeee1 <pace.alessandro05@protonmail.com>
2023-10-14 09:43:44 +0200

    added qrcode to scan menu and open webview

--- lib/ui/pages/room/menu.dart
+++ lib/ui/pages/room/menu.dart
@@ -1,12 +1,17 @@
 import 'dart:math';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:sushi_room/ui/components/extended_fab.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 import 'package:universal_io/io.dart';
 import 'package:sushi_room/utils/globals.dart' as globals;
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_universal/webview_universal.dart' as web;
 
 class MenuPage extends StatefulWidget {
   const MenuPage({
@@ -26,11 +31,54 @@ class _MenuPageState extends State<MenuPage> with AutomaticKeepAliveClientMixin 
   bool isUrl = false;
   String url = '';
 
+  WebViewController? webViewController;
+  bool scanningCode = false;
+
+  web.WebViewController? webWebViewController;
+
+  MobileScannerController? qrScanController;
+
+  void updateWebViewUrl(url) async {
+    isUrl = true;
+    webViewController?.loadRequest(
+      Uri.parse(url),
+    );
+
+    if (kIsWeb) {
+      if (webWebViewController!.is_init) {
+        webWebViewController!.go(uri: Uri.parse(url));
+      } else {
+        webWebViewController!.init(
+          context: context,
+          setState: setState,
+          uri: Uri.parse(url),
+        );
+      }
+    }
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (!kIsWeb) {
+      webViewController = WebViewController()
+        ..setJavaScriptMode(
+          JavaScriptMode.unrestricted,
+        );
+    } else {
+      webWebViewController = web.WebViewController();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    bool somethingOn = (menu == null && (url.isEmpty && !isUrl)) && !scanningCode;
+
     super.build(context);
     return Scaffold(
-      body: menu == null
+      body: somethingOn
           ? Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -54,24 +102,97 @@ class _MenuPageState extends State<MenuPage> with AutomaticKeepAliveClientMixin 
                 ],
               ),
             )
-          : !isUrl
-              ? SfPdfViewer.file(
-                  menu!,
+          : scanningCode
+              ? MobileScanner(
+                  controller: qrScanController,
+                  onDetect: (capture) {
+                    for (var element in capture.barcodes) {
+                      if (element.format == BarcodeFormat.qrCode && element.rawValue != null) {
+                        try {
+                          Uri.parse(element.rawValue!);
+                        } catch (e) {
+                          debugPrint("Invalid url ${element.rawValue}");
+                          return;
+                        }
+
+                        url = element.rawValue!;
+                        isUrl = true;
+                        scanningCode = false;
+                        updateWebViewUrl(url);
+                        qrScanController?.stop();
+
+                        setState(() {});
+                      }
+                    }
+                  },
                 )
-              : SfPdfViewer.network(
-                  menu!.path,
-                ),
-      floatingActionButton: menu == null
+              : !isUrl
+                  ? SfPdfViewer.file(
+                      menu!,
+                    )
+                  : !kIsWeb
+                      ? WebViewWidget(
+                          controller: webViewController!,
+                          gestureRecognizers: {}..add(Factory<OneSequenceGestureRecognizer>(() => EagerGestureRecognizer())),
+                        )
+                      : const Center(
+                          child: Text("Menu is not supported on web yet :/"),
+                        ),
+      //
+      floatingActionButton: somethingOn
           ? ExpandableFab(
               distance: 80,
               children: [
-                const ActionButton(
-                  onPressed: null,
-                  icon: Icon(Icons.link),
+                ActionButton(
+                  onPressed: () async {
+                    //dialog promting for url
+                    await showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: Text(
+                          FlutterI18n.translate(context, 'menuView.title'),
+                        ),
+                        content: TextField(
+                          onChanged: (value) {
+                            url = value;
+                          },
+                          decoration: InputDecoration(
+                            hintText: FlutterI18n.translate(context, 'menuView.urlHint'),
+                          ),
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text(
+                              FlutterI18n.translate(context, 'cancel'),
+                              style: Theme.of(context).textTheme.bodyLarge,
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              updateWebViewUrl(url);
+                              Navigator.pop(context);
+                            },
+                            child: Text(
+                              FlutterI18n.translate(context, 'ok'),
+                              style: Theme.of(context).textTheme.bodyLarge,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  icon: const Icon(Icons.link),
                 ),
-                const ActionButton(
-                  onPressed: null,
-                  icon: Icon(Icons.qr_code_scanner),
+                ActionButton(
+                  onPressed: () {
+                    scanningCode = true;
+                    qrScanController = MobileScannerController();
+                    setState(() {});
+                  },
+                  icon: const Icon(Icons.qr_code_scanner),
                 ),
                 ActionButton(
                   onPressed: () async {
@@ -93,6 +214,12 @@ class _MenuPageState extends State<MenuPage> with AutomaticKeepAliveClientMixin 
           : FloatingActionButton(
               onPressed: () {
                 menu = null;
+                isUrl = false;
+                url = '';
+
+                scanningCode = false;
+                qrScanController?.dispose();
+
                 setState(() {});
               },
               child: const Icon(Icons.close),
